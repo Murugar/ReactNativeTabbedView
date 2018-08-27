adf246e2f1ff089f5996946ba0bfadaae22acb4b
Davanon <davanon2014@gmail.com>
2018-08-27 17:46:37 +0530

    First

---
+++ App.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import { StyleSheet, View, Button, Text} from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import Login from './LoginVC';
+import Tab from './Component/Tab';
+export default class App extends Component {
+  render() {
+    return (
+      <RootStack/>
+    );
+  }
+}
+class Preview extends Component{
+  render(){
+     return(
+      <View style = {styles.container}>
+        
+        <Button
+        title="Login"
+        onPress={() => this.props.navigation.navigate('LoginST')}
+        >
+        </Button>
+      </View>
+    );
+  }
+}
+const LoginStack = createStackNavigator(
+  {
+    LoginVC: Login,
+    TabVC: Tab,
+  },
+  {
+    initialRouteName: 'LoginVC',
+    headerMode: 'none',
+  }
+);
+const RootStack = createStackNavigator(
+  {
+    PreviewVC: Preview,
+    LoginST: LoginStack,
+  },
+  {
+    initialRouteName: 'PreviewVC',
+    headerMode: 'none', 
+  }
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
---
+++ Component/Tab/HomeTab.js
@@ -0,0 +1,31 @@
+import React, {Component }from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+} from 'react-native';
+import {Icon} from 'native-base';
+
+class HomeTab extends Component{
+    static navigationOptions = {
+        tabBarIcon: ({ tintColor }) => (
+            < Icon name = 'ios-heart' style={{color:tintColor}} />
+        )
+    }
+    render(){
+        return(
+            <View style = {style.container}>
+                <Text> Home </Text>
+            </View>
+        );
+    }
+}
+
+export default HomeTab;
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
